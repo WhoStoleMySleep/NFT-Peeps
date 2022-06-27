f3104967f6bbcc7e34ec315057cc9f654ce34bee
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-06-28 01:03:09 +0700

    feat(components): created the 'Header' component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import './Header.scss'
+import Button from '../Button/Button';
+import logo from '../../assets/images/logo.svg'
+
+const Header = () => {
+  const onVievMenu = () => {
+    const header = document.querySelector('.js-header')
+
+    header?.classList.toggle('open')
+  }
+  
+  return (
+    <header className='header js-header'>
+      <nav className='header__menu'>
+        <img src={logo} alt='logo' className='header__logo' onClick={onVievMenu}/>
+        <ul className='header__link-list'>
+          <li className='header__link'>
+            <a className='header__about-me' href='/mock-address/Collection'>Collection</a>
+          </li>
+          <li className='header__link'>
+            <a className='header__job-opening' href='/mock-address/Content'>Content</a>
+          </li>
+          <li className='header__link'>
+            <a className='header__news' href='/mock-address/Roadmap'>Roadmap</a>
+          </li> 
+          <li className='header__link'>
+            <a className='header__team' href='/mock-address/Team'>Team</a>
+          </li> 
+        </ul>
+        <Button text='OpenSea' />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
