5022a420587487d40687ecf7a5eadbec99cf7a88
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-06-28 17:55:19 +0700

    feat(components):create the 'Collection' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import Collection from './components/Collection/Collection';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Header />   
       <Hero />
+      <Collection />
     </>
   );
 }
---
+++ src/components/Collection/Collection.tsx
@@ -0,0 +1,48 @@
+import './Collection.scss'
+import group from '../../assets/images/group.png'
+
+const Collection = () => {
+  return (
+    <section className='collection'>
+      <p className='collection__upper'>
+        Collection
+      </p>
+      <h2 className='collection__headed'>
+        Meet Our Bored Peeps!
+      </h2>
+      <div className='collection__row'>
+        <ul className='collection__list-col'>
+          <li className='collection__list-item'>
+            <h4 className='collection__list-headed'>6500 Bored Peeps</h4>
+            <p className='collection__list-additional'>
+              Tempor interdum sed nisl aliquam ut orci feugiat neque.
+              Nibh amet proin in tristique.
+              Ipsum morbi adipiscing faucibus eu odio in tellus risus.
+            </p>
+          </li>
+          <li className='collection__list-item'>
+            <h4 className='collection__list-headed'>Mint Price & Date</h4>
+            <p className='collection__list-additional'>
+              Tempor interdum sed nisl aliquam ut orci feugiat neque.
+              Nibh amet proin in tristique.
+              Ipsum morbi adipiscing faucibus eu odio in tellus risus.
+            </p>
+          </li>
+          <li className='collection__list-item'>
+            <h4 className='collection__list-headed'>Why Bored Peeps</h4>
+            <p className='collection__list-additional'>
+              Tempor interdum sed nisl aliquam ut orci feugiat neque.
+              Nibh amet proin in tristique.
+              Ipsum morbi adipiscing faucibus eu odio in tellus risus.
+            </p>
+          </li>
+        </ul>
+        {window.screen.width > 890 &&
+          <img src={group} alt='' className='collection__image' />
+        }
+      </div>
+    </section>
+  );
+}
+
+export default Collection;
