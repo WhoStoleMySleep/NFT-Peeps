cffbb81a96bd81227a3b4325b2c22cb2d3d0ee93
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-06-30 11:50:10 +0700

    feat(components): create a 'Team' component

---
+++ src/components/Team/Team.tsx
@@ -0,0 +1,60 @@
+import TeamBlock from '../TeamBlock/TeamBlock';
+import './Team.scss'
+import jennifer from '../../assets/images/jennifer.png'
+import matthew from '../../assets/images/matthew.png'
+import john from '../../assets/images/john.png'
+import justin from '../../assets/images/justin.png'
+import jessica from '../../assets/images/jessica.png'
+import katty from '../../assets/images/katty.png'
+
+const Team = () => {
+  const teamBlockInfo = [
+    {
+      img: jennifer,
+      profession: 'CEO',
+      name: 'Jennifer',
+    },
+    {
+      img: matthew,
+      profession: 'Developer',
+      name: 'Matthew',
+    },
+    {
+      img: john,
+      profession: 'Developer',
+      name: 'John',
+    },
+    {
+      img: justin,
+      profession: 'Designer',
+      name: 'Justin',
+    },
+    {
+      img: jessica,
+      profession: 'Designer',
+      name: 'Jessica',
+    },
+    {
+      img: katty,
+      profession: 'Designer',
+      name: 'Katty',
+    },
+  ]
+
+  return (
+    <section className='team'>
+      <p className='team__upper'>Team</p>
+      <h2 className='team__headed'>Meet Our Team</h2>
+      <ul className='team__developer-container'>
+        <TeamBlock {...teamBlockInfo[0]} />
+        <TeamBlock {...teamBlockInfo[1]} />
+        <TeamBlock {...teamBlockInfo[2]} />
+        <TeamBlock {...teamBlockInfo[3]} />
+        <TeamBlock {...teamBlockInfo[4]} />
+        <TeamBlock {...teamBlockInfo[5]} />
+      </ul>
+    </section>
+  );
+}
+
+export default Team;
