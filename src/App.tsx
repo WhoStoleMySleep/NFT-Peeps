2af6c597c7ed8291cc8091fe9663b884008ec646
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2022-07-01 01:32:10 +0700

    refactor(components): import sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.scss'
-import Collection from './components/Collection/Collection';
-import Content from './components/Content/Content';
-import Faq from './components/Faq/Faq';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Collection from './components/Collection/Collection';
+import Content from './components/Content/Content';
 import Team from './components/Team/Team';
+import Faq from './components/Faq/Faq';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
